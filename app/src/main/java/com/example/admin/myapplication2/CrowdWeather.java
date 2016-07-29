62f59fc946e61fc2c69afec67181e6cde6a921c7
zsy2053 <zsy2053@gmail.com>
2016-07-29 03:41:17 -0400

    Creating Cloud Server

---
+++ app/src/main/java/com/example/admin/myapplication2/CrowdWeather.java
@@ -0,0 +1,19 @@
+package com.example.admin.myapplication2;
+
+import com.firebase.client.Firebase;
+import com.google.firebase.FirebaseApp;
+import com.google.firebase.database.FirebaseDatabase;
+
+/**
+ * Created by admin on 2016/7/29.
+ */
+public class CrowdWeather extends android.app.Application {
+    @Override
+    public void onCreate() {
+        super.onCreate();
+        if (!FirebaseApp.getApps(this).isEmpty()) {
+            FirebaseDatabase.getInstance().setPersistenceEnabled(true);
+        }
+        Firebase.setAndroidContext(this);
+    }
+}
--- app/src/main/java/com/example/admin/myapplication2/MainActivity.java
+++ app/src/main/java/com/example/admin/myapplication2/MainActivity.java
@@ -8,9 +8,21 @@ import android.support.v7.widget.Toolbar;
 import android.view.View;
 import android.view.Menu;
 import android.view.MenuItem;
+import android.widget.Button;
+import android.widget.TextView;
+
+import com.firebase.client.DataSnapshot;
+import com.firebase.client.Firebase;
+import com.firebase.client.FirebaseError;
+import com.firebase.client.ValueEventListener;
+
 
 public class MainActivity extends AppCompatActivity {
 
+    TextView mTextFieldCondition;
+    Button mButtonSunny;
+    Button mButtonFoggy;
+    Firebase mRef;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
@@ -18,12 +30,34 @@ public class MainActivity extends AppCompatActivity {
         Toolbar toolbar = (Toolbar) findViewById(R.id.toolbar);
         setSupportActionBar(toolbar);
 
+
         FloatingActionButton fab = (FloatingActionButton) findViewById(R.id.fab);
         fab.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View view) {
                 Snackbar.make(view, "Replace with your own action", Snackbar.LENGTH_LONG)
                         .setAction("Action", null).show();
+            }
+        });
+    }
+    @Override
+    protected void onStart() {
+        super.onStart();
+        mTextFieldCondition = (TextView)findViewById(R.id.textView);
+        mButtonFoggy = (Button)findViewById(R.id.button2);
+        mButtonSunny = (Button)findViewById(R.id.button);
+        mRef = new Firebase("https://chatanywhere-1469761434742.firebaseio.com/condition/");
+
+        mRef.addValueEventListener(new ValueEventListener() {
+            @Override
+            public void onDataChange(DataSnapshot dataSnapshot) {
+                     String text = dataSnapshot.getValue(String.class);
+                     mTextFieldCondition.setText(text);
+            }
+
+            @Override
+            public void onCancelled(FirebaseError firebaseError) {
+
             }
         });
     }
